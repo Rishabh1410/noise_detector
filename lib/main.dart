64d891b2f2a70ad9dc70224d86077e898d2eb506
Rishabh1410 <60822949+Rishabh1410@users.noreply.github.com>
2020-12-20 19:33:14 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
 
 class Post {
   dynamic sound;
@@ -39,7 +40,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
-  String myText = "0";
+  String myText = "40";
 
   static final CREATE_POST_URL = 'http://192.168.4.1/convertDataToJson';
 
@@ -59,6 +60,7 @@ class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
 
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "WEB SERVICE",
       theme: ThemeData(
         primaryColor: Colors.teal,
@@ -88,28 +90,37 @@ class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  new Text(
-                    "${myText}",
-                    style: TextStyle(
-                      fontSize: 45,
-                      fontFamily: 'Bitter-Regular',
-                    ),
-                  ),
-                  new Column(
-                    children: <Widget>[
-                      new Row(
-                        children: <Widget>[
-                          new Text(
-                            "db",
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontFamily: 'Bitter-Regular',
-                            ),
-                          ),
+                  SfRadialGauge(axes: <RadialAxis>[
+                    RadialAxis(
+                        minimum: 0,
+                        maximum: 150,
+                        interval: 10,
+                        ranges: <GaugeRange>[
+                          GaugeRange(
+                              startValue: 0, endValue: 50, color: Colors.green),
+                          GaugeRange(
+                              startValue: 50,
+                              endValue: 100,
+                              color: Colors.orangeAccent),
+                          GaugeRange(
+                              startValue: 100, endValue: 150, color: Colors.red)
                         ],
-                      )
-                    ],
-                  ),
+                        pointers: <GaugePointer>[
+                          NeedlePointer(
+                              value: double.parse('$myText'),
+                              enableAnimation: true)
+                        ],
+                        annotations: <GaugeAnnotation>[
+                          GaugeAnnotation(
+                              widget: Text('$myText' + 'db',
+                                  style: TextStyle(
+                                      color: Colors.red,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 25)),
+                              positionFactor: 0.3,
+                              angle: 90)
+                        ]),
+                  ]),
                 ],
               ),
               new SizedBox(
